1ca8200eca5f094db7c162b17ab9030e5cfdd386
Sharm-Dev <sharmainetaylor@ymail.com>
2022-02-06 18:48:16 +0000

    created js file and added my first line of code in a h1 tag and used the document getElementById to append to the DOM

---
+++ index.js
@@ -0,0 +1,5 @@
+//Task 1
+// Spend a minute memorizing the line below
+// Re-write the line of code as best you can from memory
+
+ReactDOM.render(<h1>Hi World!!!</h1>, document.getElementById("root"))
