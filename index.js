bb1d3215067c431e39a1028314e518ce5a604617
Sharm-Dev <sharmainetaylor@ymail.com>
2022-02-07 21:32:51 +0000

    console.log the variable with the JSX to show what the comsole displayed, this shows a object and in the children propertys there are React elements

--- index.js
+++ index.js
@@ -5,12 +5,16 @@
 
 // JSX
 const page = (
-    <div>
-        <h1 className="header">This is JSX</h1>
-        <p>This is a paragraph</p>
-    </div>
-)
-ReactDOM.render(
-    page,
-    document.getElementById("root")
-)
+  <div>
+    <h1 className="header">This is JSX</h1>
+    <p>This is a paragraph</p>
+  </div>
+);
+
+console.log(page);
+
+ReactDOM.render(page, document.getElementById("root"));
+
+//Below is the result from the console.log
+
+{type: "div", key: null, ref: null, props: {children: [{type: "h1", key: null, ref: null, props: {className: "header", children: "This is JSX"}, _owner: null, _store: {}}, {type: "p", key: null, ref: null, props: {children: "This is a paragraph"}, _owner: null, _store: {}}]}, _owner: null, _store: {}}
