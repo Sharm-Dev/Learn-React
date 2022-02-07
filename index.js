34d646642821aa421e0ac77f072bad0367e4beb8
Sharm-Dev <sharmainetaylor@ymail.com>
2022-02-07 21:42:57 +0000

    challenge was to create a navbar in JSX,I did this by saving my JSX in a variable

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-// const h1 = document.createElement("h1")
-// h1.textContent = "Hello world"
-// h1.className = "header"
-// console.log(h1)
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
 
-// JSX
+/* 
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+
+
 const page = (
-  <div>
-    <h1 className="header">This is JSX</h1>
-    <p>This is a paragraph</p>
-  </div>
+    <nav>
+        <h1>New Website</h1>
+        <ul>
+            <li>About</li>
+            <li>Pricing</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 );
 
-console.log(page);
-
-ReactDOM.render(page, document.getElementById("root"));
-
-//Below is the result from the console.log
-
-{type: "div", key: null, ref: null, props: {children: [{type: "h1", key: null, ref: null, props: {className: "header", children: "This is JSX"}, _owner: null, _store: {}}, {type: "p", key: null, ref: null, props: {children: "This is a paragraph"}, _owner: null, _store: {}}]}, _owner: null, _store: {}}
+ReactDOM.render(
+    page,document.getElementById("root")
+)
