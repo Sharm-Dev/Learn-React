25541fd206ef9a2ac17479bc46a24f4761299cce
Sharm-Dev <sharmainetaylor@ymail.com>
2022-02-13 23:55:39 +0000

    Refreshed files and folders to start new challenge

--- my-app/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import "../App.css";
-
-export default function Footer() {
-    return (
-      <footer>
-        <small className="footer-copyright">© 2022 Sharm-Dev development. All rights reserved.</small>
-      </footer>
-    );
-}
