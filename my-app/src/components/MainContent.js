a3d52a00b66a88ee4b39e36a70ffd1d823961685
Sharm-Dev <sharmainetaylor@ymail.com>
2022-02-13 17:17:29 +0000

    Created MainContent component and put all elements inside.

---
+++ my-app/src/components/MainContent.js
@@ -0,0 +1,14 @@
+import "../App.css";
+
+export default function MainContent() {
+    return (
+      <>
+        <h1>Reasons I'm excited to learn React</h1>
+        <ol>
+          <li>Fun to learn</li>
+          <li>Enjoy the way the code is more manageable</li>
+          <li>What to understand how to use Props and Routers</li>
+        </ol>
+      </>
+    );
+}
