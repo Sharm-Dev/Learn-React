322fe6d375dcf432301c86bf3500885160934772
Sharm-Dev <sharmainetaylor@ymail.com>
2022-02-13 19:11:36 +0000

    Added classnames to elements

--- my-app/src/components/MainContent.js
+++ my-app/src/components/MainContent.js
@@ -3,8 +3,8 @@ import "../App.css";
 export default function MainContent() {
     return (
       <>
-        <h1>Reasons I'm excited to learn React</h1>
-        <ol>
+        <h1 className="heading">Reasons I'm excited to learn React</h1>
+        <ol className="mainContent-list">
           <li>Fun to learn</li>
           <li>Enjoy the way the code is more manageable</li>
           <li>What to understand how to use Props and Routers</li>
