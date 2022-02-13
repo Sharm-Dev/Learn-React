ac0f6db4dd4c9ac9ebcc4137691f08db9ac3025a
Sharm-Dev <sharmainetaylor@ymail.com>
2022-02-13 17:16:12 +0000

    Created footer component and put all elements inside

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,9 @@
+import "../App.css";
+
+export default function Footer() {
+    return (
+      <footer>
+        <small>© 2022 Sharm-Dev development. All rights reserved.</small>
+      </footer>
+    );
+}
