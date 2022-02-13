d619e88d4617e98b4589a1136f65a2291f82646d
Sharm-Dev <sharmainetaylor@ymail.com>
2022-02-13 16:01:45 +0000

    Completed challenge requirements, putting all elements in a function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,48 @@
 /**
 Challenge: 
 
-Part 1: Create a page of your own using a custom Page component
-
-It should return an ordered list with the reasons why you're
-excited to be learning React :)
-
-Render your list to the page
-
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
  */
 
 import "./App.css";
-import reactLogo from "./download.png"
+import reactLogo from "./download.png";
 
 export default function App() {
   return (
     <>
-      <img src={reactLogo} alt="React Logo" />
-      <h2>Why I'm excited to be learning React :)</h2>
+      <header>
+        <img src={reactLogo} alt="React Logo" />
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="about">About</a>
+            </li>
+            <li>
+              <a href="contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <h1>Reasons I'm excited to learn React</h1>
       <ol>
         <li>Fun to learn</li>
         <li>Enjoy the way the code is more manageable</li>
         <li>What to understand how to use Props and Routers</li>
       </ol>
+      <footer>
+        <small>© 2022 Sharm-Dev development. All rights reserved.</small>
+      </footer>
     </>
   );
 }
-
