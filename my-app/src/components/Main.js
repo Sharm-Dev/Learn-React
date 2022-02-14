746b2255fd6a8391f92892db1c0b05da9822c660
Sharm-Dev <sharmainetaylor@ymail.com>
2022-02-14 00:23:44 +0000

    removed and added new components

---
+++ my-app/src/components/Main.js
@@ -0,0 +1,7 @@
+import '../App.css';
+
+export default function Main() {
+    return (
+        <h1>Main content goes here!</h1>
+    )
+};
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,7 @@
+import '../App.css'
+
+export default function Navbar() {
+    return (
+        <h1>Navbar goes here</h1>
+    )
+};
